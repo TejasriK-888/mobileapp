31bbbc3c5fb3d59750bcfb94ef45d9671a18d796
TejasriK-888 <145789321+TejasriK-888@users.noreply.github.com>
2025-01-24 12:21:55 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,14 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services") // Add this line in Kotlin DSL style
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
-
-android {
-    namespace = "com.example.mystud"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.mystud"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+    dependencies {
+        classpath ("com.google.gms:google-services:4.4.0" ) // Add this line
     }
 }
-
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("com.google.firebase:firebase-auth:21.0.1")  // For Firebase Authentication
-    implementation("com.google.firebase:firebase-database:20.0.4")  // For Firebase Database
-}
-
